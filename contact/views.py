c2a8523e97f9796386b7078391188d0892014d3e
Divyansh Kumar Singh <102811377+singhdivyansh0009@users.noreply.github.com>
2024-09-28 10:42:02 +0530

    Update views.py

--- contact/views.py
+++ contact/views.py
@@ -8,9 +8,10 @@ class ContactView(APIView):
         serializer = ContactSerializer(data=request.data)
         if serializer.is_valid():
              # Print the validated data
+            mydata = serializer.validated_data
             print('Data from frontend:', serializer.validated_data)  # Use validated_data to get the cleaned data
             # Here you can handle the data, e.g., save it to the database or send an email
             # For demonstration, we'll just return a success message
     
-            return Response({"message": "Message received successfully!"}, status=status.HTTP_201_CREATED)
+            return Response({"message": "Message received successfully!","data":mydata}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
